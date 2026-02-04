b7da331c0f6918b338931f510a7d403ee006bdac
krishna <kondurkrishna6@gmail.com>
2026-02-04 11:17:00 +0530

    add hashset file

---
+++ hashset.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> hs=new HashSet<>();
+        hs.add(1);
+        hs.add(5);
+        hs.add(7);
+        hs.add(1);
+        System.out.println(hs);
+
+        
+    }
+    
+}
